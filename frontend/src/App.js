aadb20c829409b1b1469ae87a6179235a7623d77
mamurjonmardonov54-source <mamurjonmardonov54@gmail.com>
2025-11-16 12:23:49 +0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AccountList from './components/AccountList';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>PUBG Account Shop</h1>
+      <AccountList />
+    </div>
+  );
+}
